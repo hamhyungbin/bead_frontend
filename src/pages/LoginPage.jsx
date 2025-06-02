8eca76890685c88f444f86a902cdc87dd4a429b5
Ham Hyungbin <35593401+hamhyungbin@users.noreply.github.com>
2025-06-02 17:19:43 +0900

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,7 +8,14 @@ function LoginPage() {
 
   return (
     <Container component="main" maxWidth="xs" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
-      <Paper elevation={3} sx={{ padding: 4, width: '100%', mt: -8 }}> {/* mt로 약간 위로 조정 */}
+      <Paper 
+        elevation={3} 
+        sx={{ 
+          padding: { xs: 2, sm: 3, md: 4 }, // 화면 크기별 패딩 조정
+          width: '100%', 
+          mt: { xs: 0, sm: -8 } // 작은 화면에서는 상단 마진 제거 또는 감소
+        }}
+      >
         <LoginForm />
         <Button
           fullWidth
